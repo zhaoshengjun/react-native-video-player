92ad4c63efa6aa8534632b3cb093ca28523cc333
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-10-18 11:17:00 +1000

    add package and set source for video playing

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, AppRegistry, TextInput } from "react-native";
+import Video from "react-native-video";
+import TestVideo from "./video.mp4";
+
+// const RemoteVedio = {uri:'https://viemo.com/assets/xxx.mp4'}
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Video
+          source={TestVideo}
+          resizeMode="cover"
+          style={StyleSheet.absoluteFill}
+        />
       </View>
     );
   }
