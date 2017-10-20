45a102f20c8c8fc007140bbdf3fc2784bbd26ae5
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-10-20 10:18:58 +1000

    handle buffer

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   Text,
   View,
+  Animated,
   AppRegistry,
   Dimensions,
   TextInput
@@ -14,7 +15,9 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 export default class App extends Component {
   state = {
-    error: false
+    error: false,
+    buffering: true,
+    animated: new Animated.Value(0)
   };
 
   handleError = meta => {
@@ -31,10 +34,42 @@ export default class App extends Component {
     });
   };
 
+  handleLoadStart = () => {
+    this.triggerBufferAnimation();
+  };
+
+  triggerBufferAnimation = () => {
+    this.loopingAnimation = Animated.loop(
+      Animated.timing(this.state.animated, {
+        toValue: 1,
+        duration: 350
+      })
+    ).start();
+  };
+
+  handleBuffer = meta => {
+    meta.isBuffering && this.triggerBufferAnimation();
+
+    if (this.loopingAnimation && !meta.isBuffering) {
+      this.loopingAnimation.stopAnimation();
+    }
+
+    this.setState({
+      buffering: meta.isBuffering
+    });
+  };
+
   render() {
     const { width } = Dimensions.get("window");
     const height = width * 0.5625;
-    const { error } = this.state;
+    const { error, buffering } = this.state;
+    const interpolatedAnimation = this.state.animated.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"]
+    });
+    const rotateStyle = {
+      transform: [{ rotate: interpolatedAnimation }]
+    };
 
     return (
       <View style={styles.container}>
@@ -44,12 +79,19 @@ export default class App extends Component {
             source={{ uri: "http://google.com/notavideo" }}
             resizeMode="contain"
             onError={this.handleError}
+            onLoadStart={this.handleLoadStart}
+            onBuffer={this.handleBuffer}
           />
           <View style={styles.videoCover}>
             {error && (
               <Icon name="exclamation-triangle" size={30} color="red" />
             )}
             {error && <Text>{error}</Text>}
+            {buffering && (
+              <Animated.View style={rotateStyle}>
+                <Icon name="circle-o-notch" size={30} color="#FFF" />
+              </Animated.View>
+            )}
           </View>
         </View>
       </View>
@@ -70,7 +112,7 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     bottom: 0,
-    backgroundColor: "rgba(255,255,255,.9"
+    backgroundColor: "transparent"
   },
   error: {
     backgroundColor: "#000"
