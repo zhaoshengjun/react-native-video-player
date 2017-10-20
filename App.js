7651b6db8ff2f510b7e8b91200c8f61614799da8
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-10-20 10:43:17 +1000

    auto play/pause

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   View,
   Animated,
   AppRegistry,
+  ScrollView,
   Dimensions,
   TextInput
 } from "react-native";
@@ -13,11 +14,18 @@ import TestVideo from "./video.mp4";
 // const RemoteVedio = {uri:'https://viemo.com/assets/xxx.mp4'}
 import Icon from "react-native-vector-icons/FontAwesome";
 
+const THRESHOLD = 100; // when the video container is passing 100 pixel, the video will start to play. vise versa.
 export default class App extends Component {
   state = {
     error: false,
+    paused: true,
     buffering: true,
     animated: new Animated.Value(0)
+  };
+
+  position = {
+    start: null,
+    end: null
   };
 
   handleError = meta => {
@@ -59,6 +67,25 @@ export default class App extends Component {
     });
   };
 
+  handleScroll = evt => {
+    const scrollPosition = e.nativeEvent.contentOffset.y;
+    const paused = this.state.paused;
+    const { start, end } = this.position;
+
+    if (scrollPosition > start && scrollPosition < end && paused) {
+      this.setState({ paused: false });
+    } else if ((scrollPosition > end || scrollPosition < start) && !paused) {
+      this.setState({ paused: true });
+    }
+  };
+
+  handleVideoLayout = evt => {
+    const { height } = Dimensions.get("window");
+    this.position.start = evt.nativeEvent.layout.y - height + THRESHOLD;
+    this.position.end =
+      evt.nativeEvent.layout.y + evt.nativeEvent.layout.height - THRESHOLD;
+  };
+
   render() {
     const { width } = Dimensions.get("window");
     const height = width * 0.5625;
@@ -73,37 +100,43 @@ export default class App extends Component {
 
     return (
       <View style={styles.container}>
-        <View style={error ? sytles.error : undefined}>
-          <Video
-            style={StyleSheet.absoluteFill}
-            source={{ uri: "http://google.com/notavideo" }}
-            resizeMode="contain"
-            onError={this.handleError}
-            onLoadStart={this.handleLoadStart}
-            onBuffer={this.handleBuffer}
-            repeat
-          />
-          <View style={styles.videoCover}>
-            {error && (
-              <Icon name="exclamation-triangle" size={30} color="red" />
-            )}
-            {error && <Text>{error}</Text>}
-            {buffering && (
-              <Animated.View style={rotateStyle}>
-                <Icon name="circle-o-notch" size={30} color="#FFF" />
-              </Animated.View>
-            )}
-          </View>
-          <View>
-            <Text style={styles.header}>Login</Text>
-            <TextInput placeholder="Email" style={styles.input} />
-            <TextInput
-              placeholder="Password"
-              secureTextEntry
-              style={styles.input}
+        <ScrollView scrollEventThrottle={16} onScroll={this.handleScroll}>
+          <View style={styles.fakeContent} />
+          <View style={error ? sytles.error : undefined}>
+            <Video
+              style={StyleSheet.absoluteFill}
+              source={{ uri: "http://google.com/notavideo" }}
+              resizeMode="contain"
+              onError={this.handleError}
+              onLoadStart={this.handleLoadStart}
+              onBuffer={this.handleBuffer}
+              repeat
+              paused={this.state.paused}
+              onLayout={this.handleVideoLayout}
             />
+            <View style={styles.videoCover}>
+              {error && (
+                <Icon name="exclamation-triangle" size={30} color="red" />
+              )}
+              {error && <Text>{error}</Text>}
+              {buffering && (
+                <Animated.View style={rotateStyle}>
+                  <Icon name="circle-o-notch" size={30} color="#FFF" />
+                </Animated.View>
+              )}
+            </View>
+            <View>
+              <Text style={styles.header}>Login</Text>
+              <TextInput placeholder="Email" style={styles.input} />
+              <TextInput
+                placeholder="Password"
+                secureTextEntry
+                style={styles.input}
+              />
+            </View>
           </View>
-        </View>
+          <View style={styles.fakeContent} />
+        </ScrollView>
       </View>
     );
   }
@@ -140,3 +173,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#000"
   }
 });
+
+AppRegistry.registerComponent("rn-video", () => App);
